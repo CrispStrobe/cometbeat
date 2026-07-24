9ff10c524acf17e534e36f44ec088f3024c84b4d
CrispStrobe <CrispStrobe@users.noreply.github.com>
2026-07-24 13:21:34 +0000

    Deploy web build to Pages (d5672169a937a62f84e4f13424f0c71ed479cf38) d5672169a937a62f84e4f13424f0c71ed479cf38

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3268520308" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1635424928" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
